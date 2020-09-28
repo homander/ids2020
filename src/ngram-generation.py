0df7ad36d03d56a8a0646284d0f8e502a98316a6
Hohtio, Jani <jani.hohtio@accenture.com>
2020-09-28 18:12:18 +0300

    Added one-gram skill lists

--- src/ngram-generation.py
+++ src/ngram-generation.py
@@ -47,17 +47,17 @@ temp = jds['description_sw'].apply(pd.Series)
 word_list = temp.stack().to_list()
 
 # produce 4-, 3-, 2- and 1-grams
-four_gram = (pd.Series(nltk.ngrams(word_list, 4)).value_counts())[:2000]
+four_gram = (pd.Series(nltk.ngrams(word_list, 4)).value_counts())[:4000]
 four_gram_df = pd.DataFrame(four_gram)
 four_gram_df.to_csv('processed-data/gd-jobdescription-four-gram-counts.csv')
 four_gram_df.to_excel('processed-data/gd-jobdescription-four-gram-counts.xlsx')
 
-three_gram = (pd.Series(nltk.ngrams(word_list, 3)).value_counts())[:2000]
+three_gram = (pd.Series(nltk.ngrams(word_list, 3)).value_counts())[:4000]
 three_gram_df = pd.DataFrame(three_gram)
 three_gram_df.to_csv('processed-data/gd-jobdescription-three-gram-counts.csv')
 three_gram_df.to_excel('processed-data/gd-jobdescription-three-gram-counts.xlsx')
 
-two_gram = (pd.Series(nltk.ngrams(word_list, 2)).value_counts())[:2000]
+two_gram = (pd.Series(nltk.ngrams(word_list, 2)).value_counts())[:4000]
 two_gram_df = pd.DataFrame(two_gram)
 two_gram_df.to_csv('processed-data/gd-jobdescription-two-gram-counts.csv')
 two_gram_df.to_excel('processed-data/gd-jobdescription-two-gram-counts.xlsx')
