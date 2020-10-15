40d80d97d37cca03060f4d47229993da5f760cde
Artem Stepanenko <algo.artem@gmail.com>
2020-10-15 15:18:25 +0300

    ML - with X.csv and correctly processed pipeline

--- src/Additional cleaning and ML.py
+++ src/Additional cleaning and ML.py
@@ -1,12 +1,16 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
 from nltk import WordNetLemmatizer, RegexpTokenizer, SnowballStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import LabelEncoder
+import statsmodels.api as sm
 
 # Import preprocessed CSV file
-df = pd.read_csv('processed-data/df_cleaned_for_visualisation.csv', index_col=0)
+df = pd.read_csv('processed-data/df_cleaned.csv', index_col=0)
+df_X = pd.read_csv('processed-data/2020-X.csv', index_col=0)
 
 #############################
 # ADDITIONAL DATA WRANGLING #
@@ -31,60 +35,13 @@ df['Type of ownership_le'] = le.fit_transform(df['Type of ownership'])
 
 onehotencoder = OneHotEncoder()
 
+cols = ['Avg Salary', 'Company Name_le', 'Location_City_le', 'Job Type_le', 'Industry_le', 'Location_State_le', 'Type of ownership_le','Rating']
+hm = sns.heatmap(df[cols].corr(), cbar=True, annot=True)
+plt.show()
 
 ###########
 # ML PART #
 ###########
-
-# Check R squared correlation coefficient between Company's rating and average salary
-X = df['Rating'].values.reshape(-1, 1)
-X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
-y = df['Avg Salary'].values.reshape(-1, 1)
-y_model = LinearRegression().fit(X, y)
-print('Correlations')
-print(f"The R squared correlation coefficient between Company's rating and salary is: {y_model.score(X, y)}")
-
-# Check R squared correlation coefficient between Company's rating and average salary
-X = df['Type of ownership_le'].values.reshape(-1, 1)
-X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
-y = df['Avg Salary'].values.reshape(-1, 1)
-y_model = LinearRegression().fit(X, y)
-print(f"The R squared correlation coefficient between type of ownership and salary is: {y_model.score(X, y)}")
-
-# Check R squared correlation coefficient between industry and average salary
-X = df['Industry_le'].values.reshape(-1, 1)
-X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
-y = df['Avg Salary'].values.reshape(-1, 1)
-y_model = LinearRegression().fit(X, y)
-print(f"The R squared correlation coefficient between industry and salary is: {y_model.score(X, y)}")
-
-# Check R squared correlation coefficient between sector and average salary
-X = df['Sector_le'].values.reshape(-1, 1)
-X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
-y = df['Avg Salary'].values.reshape(-1, 1)
-y_model = LinearRegression().fit(X, y)
-print(f"The R squared correlation coefficient between sector and salary is: {y_model.score(X, y)}")
-
-# Check R squared correlation coefficient between company name and average salary
-X = df['Company Name_le'].values.reshape(-1, 1)
-X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
-y = df['Avg Salary'].values.reshape(-1, 1)
-y_model = LinearRegression().fit(X, y)
-print(f"The R squared correlation coefficient between company name and salary is: {y_model.score(X, y)}")
-
-# Check R squared correlation coefficient between city and average salary
-X = df['Location_City_le'].values.reshape(-1, 1)
-X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
-y = df['Avg Salary'].values.reshape(-1, 1)
-y_model = LinearRegression().fit(X, y)
-print(f"The R squared correlation coefficient between city and salary is: {y_model.score(X, y)}")
-
-# Check R squared correlation coefficient between state and average salary
-X = df['Location_State_le'].values.reshape(-1, 1)
-X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
-y = df['Avg Salary'].values.reshape(-1, 1)
-y_model = LinearRegression().fit(X, y)
-print(f"The R squared correlation coefficient between state and salary is: {y_model.score(X, y)}")
 
 # Check R squared correlation coefficient between job type and average salary
 X = df['Job Type_le'].values.reshape(-1, 1)
@@ -92,6 +49,66 @@ X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
 y = df['Avg Salary'].values.reshape(-1, 1)
 y_model = LinearRegression().fit(X, y)
 print(f"The R squared correlation coefficient between job type and salary is: {y_model.score(X, y)}")
+#print(sm.OLS(y, sm.add_constant(X)).fit().summary())
+# sns.regplot(x="Job Type_le", y='Avg Salary', data=df)
+# plt.show()
+
+# Check R squared correlation coefficient between city and average salary
+X = df['Location_City_le'].values.reshape(-1, 1)
+X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
+y = df['Avg Salary'].values.reshape(-1, 1)
+y_model = LinearRegression().fit(X, y)
+print('Correlations')
+print(f"The R squared correlation coefficient between city and salary is: {y_model.score(X, y)}")
+#print(sm.OLS(y, sm.add_constant(X)).fit().summary())
+
+# Check R squared correlation coefficient between Company's rating and average salary
+X = df['Rating'].values.reshape(-1, 1)
+X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
+y = df['Avg Salary'].values.reshape(-1, 1)
+y_model = LinearRegression().fit(X, y)
+print(f"The R squared correlation coefficient between Company's rating and salary is: {y_model.score(X, y)}")
+#print(sm.OLS(y, sm.add_constant(X)).fit().summary())
+
+# Check R squared correlation coefficient between Type of ownership and average salary
+X = df['Type of ownership_le'].values.reshape(-1, 1)
+X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
+y = df['Avg Salary'].values.reshape(-1, 1)
+y_model = LinearRegression().fit(X, y)
+print(f"The R squared correlation coefficient between type of ownership and salary is: {y_model.score(X, y)}")
+#print(sm.OLS(y, sm.add_constant(X)).fit().summary())
+
+# Check R squared correlation coefficient between industry and average salary
+X = df['Industry_le'].values.reshape(-1, 1)
+X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
+y = df['Avg Salary'].values.reshape(-1, 1)
+y_model = LinearRegression().fit(X, y)
+print(f"The R squared correlation coefficient between industry and salary is: {y_model.score(X, y)}")
+#print(sm.OLS(y, sm.add_constant(X)).fit().summary())
+
+# Check R squared correlation coefficient between sector and average salary
+X = df['Sector_le'].values.reshape(-1, 1)
+X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
+y = df['Avg Salary'].values.reshape(-1, 1)
+y_model = LinearRegression().fit(X, y)
+print(f"The R squared correlation coefficient between sector and salary is: {y_model.score(X, y)}")
+#print(sm.OLS(y, sm.add_constant(X)).fit().summary())
+
+# Check R squared correlation coefficient between company name and average salary
+X = df['Company Name_le'].values.reshape(-1, 1)
+X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
+y = df['Avg Salary'].values.reshape(-1, 1)
+y_model = LinearRegression().fit(X, y)
+print(f"The R squared correlation coefficient between company name and salary is: {y_model.score(X, y)}")
+#print(sm.OLS(y, sm.add_constant(X)).fit().summary())
+
+# Check R squared correlation coefficient between state and average salary
+X = df['Location_State_le'].values.reshape(-1, 1)
+X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
+y = df['Avg Salary'].values.reshape(-1, 1)
+y_model = LinearRegression().fit(X, y)
+print(f"The R squared correlation coefficient between state and salary is: {y_model.score(X, y)}")
+#print(sm.OLS(y, sm.add_constant(X)).fit().summary())
 
 # Check R squared correlation coefficient between city+job+company and average salary
 X = df[['Job Type_le', 'Location_City_le', 'Company Name_le']]
@@ -99,6 +116,8 @@ X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
 y = df['Avg Salary'].values.reshape(-1, 1)
 y_model = LinearRegression().fit(X, y)
 print(f"The R squared correlation coefficient between job+city+company and salary is: {y_model.score(X, y)}")
+#print(sm.OLS(y, sm.add_constant(X)).fit().summary())
+
 
 # Check R squared correlation coefficient between state+city+job+industry+company and average salary
 X = df[['Job Type_le', 'Location_State_le', 'Location_City_le', 'Company Name_le']]
@@ -106,6 +125,7 @@ X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
 y = df['Avg Salary'].values.reshape(-1, 1)
 y_model = LinearRegression().fit(X, y)
 print(f"The R squared correlation coefficient between job+state+city+company and salary is: {y_model.score(X, y)}")
+#print(sm.OLS(y, sm.add_constant(X)).fit().summary())
 
 ########################################
 # SKILLS REQUIRED ADDITIONAL WRANGLING #
@@ -117,96 +137,55 @@ tokenizer = RegexpTokenizer(r'\S+')
 df['Skills'] = df['Job Description'].apply(lambda x: tokenizer.tokenize(x.lower()))
 print('Tokenizing OK')
 
-'''
-# Removing stopwords
-print('Removing stopwords - takes around 20min...', end=' ')
-def remove_stopwords(text):
-    return [word for word in text if word not in stopwords.words('english')]
-df['Skills'] = df['Skills'].apply(lambda x: remove_stopwords(x))
-print('Removing stopwords OK')
-'''
-
 # Words Lemmatization
 print('Lemmatizing...', end=' ')
 lemmatizer = WordNetLemmatizer()
+
+
 def word_lemmatizer(text):
     return " ".join([lemmatizer.lemmatize(word) for word in text])
+
+
 df['Skills_lem'] = df['Skills'].apply(lambda x: word_lemmatizer(x))
 print('Lemmatizing OK')
 
-# Stemming the words
-print('Stemming...', end=' ')
-stemmer = SnowballStemmer('english')
-def word_stemmer(text):
-    return " ".join([stemmer.stem(word) for word in text])
-df['Skills_stem'] = df['Skills'].apply(lambda x: word_stemmer(x))
-print('Steming OK')
-
-# Vectorizing skills descriptions with ngrams 3 and max_features 1000
-print('Vectorizing...', end=' ')
-vector = TfidfVectorizer(ngram_range=(2, 2), analyzer='word', max_features=2500)
-X_lem = pd.DataFrame(vector.fit_transform(df["Skills_lem"]).toarray())
-X_stem = pd.DataFrame(vector.fit_transform(df["Skills_stem"]).toarray())
-print('Vectorizing OK')
-
-# Check R squared correlation coefficient between city+job+company+description and average salary
-X = df[['Job Type_le', 'Location_City_le', 'Company Name_le']]
-X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
-X_concat = pd.concat([X, X_lem], axis=1, sort=False)
-y = df['Avg Salary'].values.reshape(-1, 1)
-y_model = LinearRegression().fit(X_concat, y)
-print(f"The R squared corr. job+city+company+description(lem) and salary is (ngram=2, feat=2500): {y_model.score(X_concat, y)}")
-
-# Check R squared correlation coefficient between city+job+company+description and average salary
-X = df[['Job Type_le', 'Location_City_le', 'Company Name_le']]
-X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
-X_concat = pd.concat([X, X_stem], axis=1, sort=False)
-y = df['Avg Salary'].values.reshape(-1, 1)
-y_model = LinearRegression().fit(X_concat, y)
-print(f"The R squared corr. job+city+company+description(stem) and salary is (ngram=2, feat=2500): {y_model.score(X_concat, y)}")
-
-# Vectorizing skills descriptions with ngrams 3 and max_features 1000
+# Vectorizing skills descriptions with ngrams 2 and max_features 3000
 print('Vectorizing...', end=' ')
 vector = TfidfVectorizer(ngram_range=(2, 2), analyzer='word', max_features=3000)
 X_lem = pd.DataFrame(vector.fit_transform(df["Skills_lem"]).toarray())
-X_stem = pd.DataFrame(vector.fit_transform(df["Skills_stem"]).toarray())
+# X_stem = pd.DataFrame(vector.fit_transform(df["Skills_stem"]).toarray())
 print('Vectorizing OK')
+# Check R squared correlation coefficient between job description and average salary
+X = X_lem
+y = df['Avg Salary'].values.reshape(-1, 1)
+y_model = LinearRegression().fit(X, y)
+print(f"The R squared corr. job description and salary is: {y_model.score(X, y)}")
+#print(sm.OLS(y, sm.add_constant(X)).fit().summary())
 
-# Check R squared correlation coefficient between city+job+company+description and average salary
+# Check R squared correlation coefficient between skills and average salary
+X = df_X
+y = df['Avg Salary'].values.reshape(-1, 1)
+y_model = LinearRegression().fit(X, y)
+print(f"The R squared corr. skills and salary is: {y_model.score(X, y)}")
+#print(sm.OLS(y, sm.add_constant(X)).fit().summary())
+
+
+# Check R squared correlation coefficient between city+job+company+job_description and average salary
 X = df[['Job Type_le', 'Location_City_le', 'Company Name_le']]
 X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
 X_concat = pd.concat([X, X_lem], axis=1, sort=False)
 y = df['Avg Salary'].values.reshape(-1, 1)
 y_model = LinearRegression().fit(X_concat, y)
-print(f"The R squared corr. job+city+company+description(lem) and salary is (ngram=2, feat=3000): {y_model.score(X_concat, y)}")
+print(
+    f"The R squared corr. job+city+company+job_description(lem) and salary is (ngram=2, feat=3000): {y_model.score(X_concat, y)}")
+#print(sm.OLS(y, sm.add_constant(X_concat)).fit().summary())
 
-# Check R squared correlation coefficient between city+job+company+description and average salary
+# Check R squared correlation coefficient between city+job+company+skills and average salary
 X = df[['Job Type_le', 'Location_City_le', 'Company Name_le']]
 X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
-X_concat = pd.concat([X, X_stem], axis=1, sort=False)
+X_concat = pd.concat([X, df_X], axis=1, sort=False)
+print(X_concat.shape)
 y = df['Avg Salary'].values.reshape(-1, 1)
 y_model = LinearRegression().fit(X_concat, y)
-print(f"The R squared corr. job+city+company+description(stem) and salary is (ngram=2, feat=3000): {y_model.score(X_concat, y)}")
-
-# Vectorizing skills descriptions with ngrams 3 and max_features 1000
-print('Vectorizing...', end=' ')
-vector = TfidfVectorizer(ngram_range=(3, 3), analyzer='word', max_features=3000)
-X_lem = pd.DataFrame(vector.fit_transform(df["Skills_lem"]).toarray())
-X_stem = pd.DataFrame(vector.fit_transform(df["Skills_stem"]).toarray())
-print('Vectorizing OK')
-
-# Check R squared correlation coefficient between city+job+company+description and average salary
-X = df[['Job Type_le', 'Location_City_le', 'Company Name_le']]
-X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
-X_concat = pd.concat([X, X_lem], axis=1, sort=False)
-y = df['Avg Salary'].values.reshape(-1, 1)
-y_model = LinearRegression().fit(X_concat, y)
-print(f"The R squared corr. job+city+company+description(lem) and salary is (ngram=3, feat=3000): {y_model.score(X_concat, y)}")
-
-# Check R squared correlation coefficient between city+job+company+description and average salary
-X = df[['Job Type_le', 'Location_City_le', 'Company Name_le']]
-X = pd.DataFrame(onehotencoder.fit_transform(X).toarray())
-X_concat = pd.concat([X, X_stem], axis=1, sort=False)
-y = df['Avg Salary'].values.reshape(-1, 1)
-y_model = LinearRegression().fit(X_concat, y)
-print(f"The R squared corr. job+city+company+description(stem) and salary is (ngram=3, feat=3000): {y_model.score(X_concat, y)}")
+print(f"The R squared corr. job+city+company+skills and salary: {y_model.score(X_concat, y)}")
+#print(sm.OLS(y, sm.add_constant(X_concat)).fit().summary())
