0c92ddae032c502d90e120850f1b80a054d150a1
Matti Riekkinen <matti.riekkinen@icloud.com>
2020-10-19 20:04:55 +0300

    Study possible overfitting

---
+++ src/ml-overfitting.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import linear_model, model_selection
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+# Fit the model with/without splitting into separate training and testing sets
+# Based on this comparison there seems to be a risk of overfitting
+
+#############
+# Load data #
+#############
+
+df = pd.read_csv('processed-data/df_cleaned_filtered.csv', index_col=0)
+X_skills = pd.read_csv('processed-data/2020-X.csv', index_col=0)
+
+# Look at a subset of the data: pick only data scientist jobs
+# (because the Theil-Sen estimator seems really slow with larger datasets)
+# (and can't use the regular OLS estimator which is fast but tends to break down)
+mask = df['Job Type'] == 'data scientist'
+df = df[mask]
+X_skills = X_skills[mask]
+
+print('df.shape:', df.shape)
+print('X_skills.shape:', X_skills.shape)
+
+###############################
+# Encode categorical features #
+###############################
+
+# The results are basically the same with just a few categorical variables
+# but we'll include more than necessary for completeness
+df_categ = df[['Location City', 'Company Name', 'Industry', 'Type of ownership']]
+enc = OneHotEncoder(drop='first').fit(df_categ)
+X_categ = pd.DataFrame(enc.transform(df_categ).toarray(), index=df_categ.index)
+
+#############################
+# Other continuous features #
+#############################
+
+X_cont = df[['Rating']]
+
+##############################################
+# Modeling: salary ~ skills + other features #
+##############################################
+
+y = df['Avg Salary']
+X = pd.concat([X_categ, X_cont, X_skills], axis=1)
+
+plt.figure(figsize=(10, 3.5))
+
+# Without train/test split
+X_train, X_test, y_train, y_test = X, X, y, y
+reg = linear_model.LinearRegression().fit(X_train, y_train)
+r2 = reg.score(X_test, y_test)
+print('When no splitting')
+print('  Estimator: OLS')
+print('  R^2:', r2)
+print('  Intercept:', reg.intercept_)
+
+plt.subplot(1, 2, 1)
+plt.scatter(reg.predict(X_test), y_test)
+plt.title(f'No train/test split (OLS, R2={r2:0.2f})')
+plt.xlabel('Predicted salary')
+plt.ylabel('Actual salary')
+plt.tight_layout()
+
+# With train/test split
+# (in this case, we use Theil-Sen estimator because OLS seems to produce huge outliers)
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y)
+reg = linear_model.TheilSenRegressor().fit(X_train, y_train)
+r2 = reg.score(X_test, y_test)
+print('When split')
+print('  Estimator: Theil-Sen')
+print('  R^2:', r2)
+print('  Intercept:', reg.intercept_)
+
+plt.subplot(1, 2, 2)
+plt.scatter(reg.predict(X_test), y_test)
+plt.title(f'With train/test split (Theil-Sen, R2={r2:0.2f})')
+plt.xlabel('Predicted salary')
+plt.ylabel('Actual salary')
+plt.tight_layout()
+
+plt.show()
